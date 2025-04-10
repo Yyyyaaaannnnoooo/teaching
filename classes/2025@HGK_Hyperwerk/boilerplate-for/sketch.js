5bae4a7a92f5e2631b706d42718d5299a5825488
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2025-04-10 14:06:05 +0200

    add more examples hyperwerk

--- classes/2025@HGK_Hyperwerk/boilerplate-for/sketch.js
+++ classes/2025@HGK_Hyperwerk/boilerplate-for/sketch.js
@@ -1,7 +1,7 @@
 
 function setup() {
     // setting up the sketch before to start drawing
-    createCanvas(innerWidth-6, innerHeight-6); // or (innerWidth, innerHeight) [full screen]
+    createCanvas(innerWidth - 6, innerHeight - 6); // or (innerWidth, innerHeight) [full screen]
     background(0)
 }
 
@@ -10,7 +10,7 @@ function draw() {
     noFill();
     stroke(255);
 
-    let h = 350;
+    let h = 150;
     let animation = sin(frameCount * 0.05)
     animation = map(animation, -1, 1, 0.5, 1.8)
     h = h * animation;
@@ -19,17 +19,22 @@ function draw() {
     animation2 = map(animation2, -1, 1, 0, 255)
 
     let animation3 = sin(frameCount * 0.05)
-    animation3 = map(animation3, -1, 1, 20, 150)
+    animation3 = map(animation3, -1, 1, 100, 250)
     let animation4 = sin(frameCount * 0.05)
     animation4 = map(animation4, -1, 1, 100, 400)
-    let animation5 = cos(frameCount * 0.01)
-    animation5 = map(animation5, -1, 1, 100, 400)
+    let animation5 = cos(frameCount * 0.001)
+    animation5 = map(animation5, -1, 1, 0, 16)
     // console.log(frameCount);
 
-    for(let i = 0; i < 100; i++){
+    for (let i = 0; i < 200; i++) {
         let redValue = map(i, 0, 50, 0, 255);
         stroke(redValue, 255 - redValue, animation2);
-        ellipse(width/2, height/2, i * 10, h)
+        push()
+        translate(width / 2, height / 2)
+        let angle = map(i, 0, 100, 0, animation5 * PI)
+        rotate(angle)
+        ellipse(0, 0, animation3 + i * 5, i * 3 + h)
+        pop()
     }
 }
 
